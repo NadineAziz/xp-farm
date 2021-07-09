006ab8bbfdae75c63ee6f53442920d4b223b0201
Nadeen Hussein <nadeen.hussein@ibm.com>
2021-07-09 15:04:16 +0200

    first test

--- complementary-DNA/complementary-DNA.js
+++ complementary-DNA/complementary-DNA.js
@@ -1,4 +1,10 @@
 function DNAStrand(dna){
+    if(dna === 'A'){
+      return dna.replace(dna,'T');
+    }else if (dna === 'T'){
+        return dna.replace(dna,'A');
+    }
+
     return dna;
 }
 
--- complementary-DNA/complementary-DNA.test.js
+++ complementary-DNA/complementary-DNA.test.js
@@ -3,6 +3,15 @@ const { DNAStrand } = require('./complementary-DNA');
 
 describe("DNA Strand", ()=>{
     it("Should return same letter", () => {
-        expect(DNAStrand('A')).toBe('A');
+        expect(DNAStrand('B')).toBe('B');
     })
+
+    it("Should flip A to T", () => {
+        expect(DNAStrand('A')).toBe('T');
+    })
+
+    it("Should flip T to A", () => {
+        expect(DNAStrand('T')).toBe('A');
+    })
+
 })
