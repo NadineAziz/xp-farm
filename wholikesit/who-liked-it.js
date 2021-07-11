f7251b96ba4a18db0df28804f4589b183ae6cd5c
Nadeen Hussein <nadeen.hussein@ibm.com>
2021-07-12 01:54:16 +0200

    solution

--- wholikesit/who-liked-it.js
+++ wholikesit/who-liked-it.js
@@ -1,6 +1,28 @@
-function whoLikesIt(names) {
-  names = [];
-  return names;
+function whoLikesIt(likes) {
+  if (likes.length === 0) {
+    return "no one likes this";
+  } else if (likes.length === 1) {
+    return likes[0] + " likes this";
+  } else if (likes.length === 2) {
+    return likes[0] + " and " + likes[1] + " like this";
+  } else if (likes.length === 3) {
+    return likes[0] + ", " + likes[1] + " and " + likes[2] + " like this";
+  } else if (likes.length === 4) {
+    return (
+      likes[0] + ", " + likes[1] + ", " + likes[2] + " and 1 other like this"
+    );
+  } else {
+    return (
+      likes[0] +
+      ", " +
+      likes[1] +
+      ", " +
+      likes[2] +
+      " and " +
+      (likes.length - 3) +
+      " others like this"
+    );
+  }
 }
 
 module.exports = {
--- wholikesit/who-liked-it.test.js
+++ wholikesit/who-liked-it.test.js
@@ -1,8 +1,44 @@
 const { whoLikesIt } = require("./who-liked-it");
 
 describe("Who Likes It Test", () => {
-  it("Return An Empty array", () => {
-    names = [];
-    expect(whoLikesIt(names)).toStrictEqual([]);
+  it("Return 'no one likes this' ", () => {
+    expect(whoLikesIt([])).toBe("no one likes this");
+  });
+  it("Give one name, should return 'name likes this' ", () => {
+    expect(whoLikesIt(["Nadeen"])).toBe("Nadeen likes this");
+  });
+  it("Give two names, should return 'name1 and name2 like this' ", () => {
+    expect(whoLikesIt(["Nadeen", "Griffin"])).toBe(
+      "Nadeen and Griffin like this"
+    );
+  });
+  it("Give three names, should return 'name1, name2, and name3 like this' ", () => {
+    expect(whoLikesIt(["Nadeen", "Griffin", "Louis"])).toBe(
+      "Nadeen, Griffin and Louis like this"
+    );
+  });
+  it("Give four names, should return 'name1, name2, name3 and 1 other like this' ", () => {
+    expect(whoLikesIt(["Nadeen", "Griffin", "Louis", "Justin"])).toBe(
+      "Nadeen, Griffin, Louis and 1 other like this"
+    );
+  });
+  it("Give five names, should return 'name1, name2, name3 and 2 others like this' ", () => {
+    expect(whoLikesIt(["Nadeen", "Griffin", "Louis", "Justin", "Sandy"])).toBe(
+      "Nadeen, Griffin, Louis and 2 others like this"
+    );
+  });
+  it("Give eight names, should return 'name1, name2, name3 and 5 others like this' ", () => {
+    expect(
+      whoLikesIt([
+        "Nadeen",
+        "Griffin",
+        "Louis",
+        "Justin",
+        "Sandy",
+        "Six",
+        "Seven",
+        "Eight",
+      ])
+    ).toBe("Nadeen, Griffin, Louis and 5 others like this");
   });
 });
