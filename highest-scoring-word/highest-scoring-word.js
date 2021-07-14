6d96723440bb22127c347bd3d73150cb72483a99
Nadeen Hussein <nadeen.hussein@ibm.com>
2021-07-14 12:42:54 +0200

    Third test - first it tested and failed

--- highest-scoring-word/highest-scoring-word.js
+++ highest-scoring-word/highest-scoring-word.js
@@ -1,16 +1,16 @@
 function highestScoringWord(word) {
-  let suma = 0;
-  let sumb = 0;
-  let sumc = 0;
-  if (word === "a") suma = 1;
-  if (word === "b") sumb = 2;
-  if (word === "c") sumc = 3;
-  console.log(suma);
-  console.log(sumb);
-  console.log(sumc);
-  return word;
+  let sum = 0;
+  if (word === "a") return (sum = 1);
+  if (word === "b") return (sum = 2);
+  if (word === "c") return (sum = 3);
+  return sum;
+}
+
+function consoleLogging(sum) {
+  return sum;
 }
 
 module.exports = {
   highestScoringWord,
+  consoleLogging,
 };
--- highest-scoring-word/highest-scoring-word.test.js
+++ highest-scoring-word/highest-scoring-word.test.js
@@ -1,21 +1,29 @@
-const { highestScoringWord } = require("./highest-scoring-word");
+const {
+  highestScoringWord,
+  consoleLogging,
+} = require("./highest-scoring-word");
 
 describe("Highest Scoring Word", () => {
-  describe("Basic functionality", () => {
-    it("Should take a letter and return same letter", () => {
-      expect(highestScoringWord("a")).toBe("a");
-    });
-  });
+  //   describe("Basic functionality", () => {
+  //     it("Should take a letter and return same letter", () => {
+  //       expect(highestScoringWord("a")).toBe("a");
+  //     });
+  //   });
 
   describe("Should give back sum of each letter", () => {
-    it("Should take letter a and on console print 1, return a", () => {
-      expect(highestScoringWord("a")).toBe("a");
+    it("Should take letter a returning 1", () => {
+      expect(highestScoringWord("a")).toBe(1);
     });
-    it("Should take letter b and on console print 2, return b", () => {
-      expect(highestScoringWord("b")).toBe("b");
+    it("Should take letter b returning 2", () => {
+      expect(highestScoringWord("b")).toBe(2);
     });
-    it("Should take letter c and on console print 3, return c", () => {
-      expect(highestScoringWord("c")).toBe("c");
+    it("Should take letter c returning 3", () => {
+      expect(highestScoringWord("c")).toBe(3);
+    });
+  });
+  describe("Should give back an accumulative counter of each letter", () => {
+    it("Should take letter aaa and on console print 3, return aaa", () => {
+      expect(highestScoringWord("aaa")).toBe(3);
     });
   });
 });
