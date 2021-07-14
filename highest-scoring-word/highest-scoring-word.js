3667a47c2d3180c2540025a393bfa2b6d6a7967f
Nadeen Hussein <nadeen.hussein@ibm.com>
2021-07-14 13:13:17 +0200

    Fifth test - second,third,fourth it and passed

--- highest-scoring-word/highest-scoring-word.js
+++ highest-scoring-word/highest-scoring-word.js
@@ -1,22 +1,27 @@
 function highestScoringWord(word) {
+  const strings = word.split(" ");
+  let map = new Map();
   let counter = 0;
-  for (let i in word) {
-    if (word[i] === "a") {
+  let max = 0;
+
+  for (let i in strings) {
+    if (strings[i].includes("a")) {
       counter += 1;
-    } else if (word[i] === "b") {
+    } else if (strings[i].includes("b")) {
       counter += 2;
-    } else if (word[i] === "c") {
+    } else if (strings[i].includes("c")) {
       counter += 3;
     }
+    map.set(strings[i], counter);
   }
-  return counter;
+
+  return maxKeyInMap(map);
 }
 
-function consoleLogging(sum) {
-  return sum;
+function maxKeyInMap(map) {
+  return [...map.entries()].reduce((x, y) => (x[1] > y[1] ? x : y))[0];
 }
 
 module.exports = {
   highestScoringWord,
-  consoleLogging,
 };
--- highest-scoring-word/highest-scoring-word.test.js
+++ highest-scoring-word/highest-scoring-word.test.js
@@ -1,7 +1,4 @@
-const {
-  highestScoringWord,
-  consoleLogging,
-} = require("./highest-scoring-word");
+const { highestScoringWord } = require("./highest-scoring-word");
 
 describe("Highest Scoring Word", () => {
   //   describe("Basic functionality", () => {
@@ -11,42 +8,53 @@ describe("Highest Scoring Word", () => {
   //   });
 
   describe("Should give back sum of each letter", () => {
-    it("Should take letter a returning 1", () => {
-      expect(highestScoringWord("a")).toBe(1);
+    it("Should take letter a returning a", () => {
+      expect(highestScoringWord("a")).toBe("a");
     });
-    it("Should take letter b returning 2", () => {
-      expect(highestScoringWord("b")).toBe(2);
+    it("Should take letter b returning b", () => {
+      expect(highestScoringWord("b")).toBe("b");
     });
-    it("Should take letter c returning 3", () => {
-      expect(highestScoringWord("c")).toBe(3);
+    it("Should take letter c returning c", () => {
+      expect(highestScoringWord("c")).toBe("c");
     });
   });
   describe("Should give back an accumulative counter of each letter", () => {
-    it("Should take characters aaa returning 3", () => {
-      expect(highestScoringWord("aaa")).toBe(3);
+    it("Should take characters aaa returning aaa", () => {
+      expect(highestScoringWord("aaa")).toBe("aaa");
     });
-    it("Should take characters bbb returning 6", () => {
-      expect(highestScoringWord("bbb")).toBe(6);
+    it("Should take characters bbb returning bbb", () => {
+      expect(highestScoringWord("bbb")).toBe("bbb");
     });
-    it("Should take characters ccc returning 9", () => {
-      expect(highestScoringWord("ccc")).toBe(9);
+    it("Should take characters ccc returning ccc", () => {
+      expect(highestScoringWord("ccc")).toBe("ccc");
     });
-    it("Should take characters abc returning 6", () => {
-      expect(highestScoringWord("abc")).toBe(6);
+    it("Should take characters abc returning abc", () => {
+      expect(highestScoringWord("abc")).toBe("abc");
     });
   });
 
   describe("Should include other letters word", () => {
     it("Should take characters f returning 0", () => {
-      expect(highestScoringWord("f")).toBe(0);
+      expect(highestScoringWord("f")).toBe("f");
     });
-    it("Should take characters abvjcsbba returning 11", () => {
-      expect(highestScoringWord("abvjcsbba")).toBe(11);
+    it("Should take characters abvjcsbba returning abvjcsbba", () => {
+      expect(highestScoringWord("abvjcsbba")).toBe("abvjcsbba");
     });
   });
   describe("Should include sentences with space", () => {
     it("Should take sentence 'xp farm' returning farm", () => {
       expect(highestScoringWord("xp farm")).toBe("farm");
+    });
+    it("Should take sentence 'man i need a taxi up to ubud' returning taxi", () => {
+      expect(highestScoringWord("man i need a taxi up to ubud")).toBe("ubud");
+    });
+    it("Should take sentence 'what time are we climbing up the volcano' returning taxi", () => {
+      expect(
+        highestScoringWord("what time are we climbing up the volcano")
+      ).toBe("volcano");
+    });
+    it("Should take sentence 'take me to semynak' returning taxi", () => {
+      expect(highestScoringWord("take me to semynak")).toBe("semynak");
     });
   });
 });
