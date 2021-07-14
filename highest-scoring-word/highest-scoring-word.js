304f1961073345299cf5a0bf6d9d692f57c12af4
Nadeen Hussein <nadeen.hussein@ibm.com>
2021-07-14 12:19:37 +0200

    second test - third it added and passed

--- highest-scoring-word/highest-scoring-word.js
+++ highest-scoring-word/highest-scoring-word.js
@@ -1,10 +1,13 @@
 function highestScoringWord(word) {
   let suma = 0;
   let sumb = 0;
+  let sumc = 0;
   if (word === "a") suma = 1;
   if (word === "b") sumb = 2;
+  if (word === "c") sumc = 3;
   console.log(suma);
   console.log(sumb);
+  console.log(sumc);
   return word;
 }
 
