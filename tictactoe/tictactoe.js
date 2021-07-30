e1a1278c430be2c46ec15af2309336d857da28c6
Nadeen Hussein <nadeen.hussein@ibm.com>
2021-07-30 11:24:27 +0200

    Draw Board: failed test and previous tests

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -1,17 +1,30 @@
 function Game() {
-  return board = ['','','','','','','','',''];
+    let board = [0,1,2,3,4,5,6,7,8];
+    console.log(drawBoard(board));
 }
 
 
 function drawBoard(board){
-    return `'  | ' ${newBoard[0]} ' | ' ${newBoard[1]} ' | ' ${newBoard[2]} '\n',
-    '----------- \n',
-    ' | ' ${newBoard[3]} ' | ' ${newBoard[4]} ' | ' ${newBoard[5]} '\n',
-    '----------- \n',
-    ' | ' ${newBoard[6]} + ' | ' ${newBoard[7]}' | ' ${newBoard[8]} '\n'` 
+    const newBoard = board.map(x => prettyPrint(x));
+    return `        |  ${newBoard[0]}  |  ${newBoard[1]}  |  ${newBoard[2]} \n
+        ----------- \n
+        |  ${newBoard[3]}  |  ${newBoard[4]}  |  ${newBoard[5]} \n
+        ----------- \n
+        |  ${newBoard[6]}  | ${newBoard[7]}   | ${newBoard[8]} \n
+    ` 
                 
 }
 
+function prettyPrint(cell){
+    if (typeof(cell) === 'number'){
+        return '';
+    } else {
+        return cell;
+    }
+}
+
+Game();
 module.exports = {
     Game,
+    drawBoard 
   };
--- tictactoe/tictactoe.test.js
+++ tictactoe/tictactoe.test.js
@@ -1,4 +1,4 @@
-const { Game } = require("./tictactoe");
+const { Game, drawBoard } = require("./tictactoe");
 
 describe("TicTacToe", () => {
   it("Board - Gives an Empty Board", () => {
@@ -10,13 +10,12 @@ describe("TicTacToe", () => {
 
     it("Draw board - draw board as a tictactoe board", () => {
         testBoard = ['','','','','','','','','']
-        expect(drawBoard(testBoard)).toEqual([
-            `'  | ' ${newBoard[0]} ' | ' ${newBoard[1]} ' | ' ${newBoard[2]} '\n',
-    '----------- \n',
-    ' | ' ${newBoard[3]} ' | ' ${newBoard[4]} ' | ' ${newBoard[5]} '\n',
-    '----------- \n',
-    ' | ' ${newBoard[6]} + ' | ' ${newBoard[7]}' | ' ${newBoard[8]} '\n'` 
-        ]);
+        expect(drawBoard(testBoard)).toEqual(`        |  ${newBoard[0]}  |  ${newBoard[1]}  |  ${newBoard[2]} \n
+        ----------- \n
+        |  ${newBoard[3]}  |  ${newBoard[4]}  |  ${newBoard[5]} \n
+        ----------- \n
+        |  ${newBoard[6]}  | ${newBoard[7]}   | ${newBoard[8]} \n
+    ` );
     });
 
 });
