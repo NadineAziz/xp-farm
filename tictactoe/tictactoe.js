f5aa7e9fb08270ae9dffb3c5c8341e5ba9ac9388
Nadeen Hussein <nadeen.hussein@ibm.com>
2021-07-30 11:13:07 +0200

    Draw table: test fail

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -1,5 +1,15 @@
 function Game() {
-   return board = ['','','','','','','','',''];
+  return board = ['','','','','','','','',''];
+}
+
+
+function drawBoard(board){
+    return `'  | ' ${newBoard[0]} ' | ' ${newBoard[1]} ' | ' ${newBoard[2]} '\n',
+    '----------- \n',
+    ' | ' ${newBoard[3]} ' | ' ${newBoard[4]} ' | ' ${newBoard[5]} '\n',
+    '----------- \n',
+    ' | ' ${newBoard[6]} + ' | ' ${newBoard[7]}' | ' ${newBoard[8]} '\n'` 
+                
 }
 
 module.exports = {
--- tictactoe/tictactoe.test.js
+++ tictactoe/tictactoe.test.js
@@ -1,10 +1,22 @@
 const { Game } = require("./tictactoe");
 
 describe("TicTacToe", () => {
-  it("Gives an Empty Board", () => {
+  it("Board - Gives an Empty Board", () => {
     expect(Game()).toEqual([]);
   });
-  it("Gives a board with empty values", () => {
+  it("Board - Gives a board with empty values", () => {
     expect(Game()).toEqual(['','','','','','','','','']);
   });
+
+    it("Draw board - draw board as a tictactoe board", () => {
+        testBoard = ['','','','','','','','','']
+        expect(drawBoard(testBoard)).toEqual([
+            `'  | ' ${newBoard[0]} ' | ' ${newBoard[1]} ' | ' ${newBoard[2]} '\n',
+    '----------- \n',
+    ' | ' ${newBoard[3]} ' | ' ${newBoard[4]} ' | ' ${newBoard[5]} '\n',
+    '----------- \n',
+    ' | ' ${newBoard[6]} + ' | ' ${newBoard[7]}' | ' ${newBoard[8]} '\n'` 
+        ]);
+    });
+
 });
