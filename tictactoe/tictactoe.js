2fffb334bc6e6d32a143ed4c413b2e2e3ee7530f
Nadeen Hussein <nadeen.hussein@ibm.com>
2021-08-01 13:36:31 +0200

    Additional refactoring

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -1,131 +1,131 @@
-let board = [0,1,2,3,4,5,6,7,8];
+let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 let player = 1;
 let movePosition;
-let state = false;
+let state = -1;
 
-function Game(board) {
+async function Game(board) {
+  console.log(`Game Board Creation: \n The game will start with player X`);
 
-    while( state === false){
-        drawBoard(board);
+  while (state === -1) {
+    console.log(drawBoard(board));
 
-        switchPlayersTurns(player);
-        
-        console.log(`Player ${player}`);
-        movePosition = randomPosition(board);
+    player = switchPlayersTurns(player);
+    console.log(`${switchPlayersCharacters(player)}'s Turn`);
+    movePosition = randomPosition(board);
 
-        board[movePosition] = switchPlayersCharacters(player);
+    board[movePosition] = switchPlayersCharacters(player);
 
-        player += 1;
-        state = isStillOnPlay(board);
+    player += 1;
+    state = isStillOnPlay(board);
+    //await wait(2000);
+  }
 
-    return board;
-    }
+  console.log(playerWonOrDraw(state, player));
+  console.log(drawBoard(board));
+}
+// function wait(timeout) {
+//     return new Promise(resolve => {
+//         setTimeout(resolve, timeout);
+//     });
+// }
+
+function playerWonOrDraw(state, player) {
+  return state === 1
+    ? `Player ${switchPlayersCharacters(player - 1)} win`
+    : "Game draw";
 }
 
 // Added x-1 because it should not exceed out of the 9th position
-function randomPosition(board){
-    let x = Math.round(Math.random() * board.length);
+function randomPosition(board) {
+  let x = Math.round(Math.random() * board.length);
 
-    while( typeof(board[x-1]) === 'string'){
-        x = Math.round(Math.random() * board.length)
-    };
+  while (typeof board[x - 1] === "string") {
+    x = Math.round(Math.random() * board.length);
+  }
 
-    return x-1;
+  return x - 1;
 }
 function switchPlayersTurns(player) {
-    if(player%2 === 1)
-         player = 1;
-     else
-         player = 2;
- 
-     return player;
- }
+  if (player % 2 == 1) player = 1;
+  else player = 2;
 
- function switchPlayersCharacters(player){
-    let char;
-    if(player === 1)
-        char = 'X'
-    else
-        char = 'O'
-    
-    return char;
- }
- function drawBoard(board){
-    const newBoard = board.map(x => prettyPrint(x));
-    return `        |  ${newBoard[0]}  |  ${newBoard[1]}  |  ${newBoard[2]} \n
+  return player;
+}
+
+function switchPlayersCharacters(player) {
+  return player === 1 ? "X" : "O";
+}
+function drawBoard(board) {
+  const newBoard = board.map((x) => prettyPrint(x));
+  return `        |  ${newBoard[0]}  |  ${newBoard[1]}  |  ${newBoard[2]} \n
         ----------- \n
         |  ${newBoard[3]}  |  ${newBoard[4]}  |  ${newBoard[5]} \n
         ----------- \n
         |  ${newBoard[6]}  | ${newBoard[7]}   | ${newBoard[8]} \n
-    `               
+    `;
 }
- function prettyPrint(cell){
-    if (typeof(cell) === 'number'){
-        return '';
-    } else {
-        return cell;
+function prettyPrint(cell) {
+  if (typeof cell === "number") {
+    return "";
+  } else {
+    return cell;
+  }
+}
+
+function isStillOnPlay(board) {
+  if (
+    isColumnFinished(board) ||
+    isRowFinished(board) ||
+    isDiagonalFinished(board)
+  )
+    return 1;
+  if (isComplete(board)) return 0;
+
+  return -1;
+}
+
+function isComplete(board) {
+  let isComplete = true;
+  for (let i = 0; i < board.length; i++) {
+    if (typeof board[i] === "number") {
+      isComplete = false;
     }
+  }
+
+  return isComplete;
 }
 
-function isStillOnPlay(board){
-    if (isColumnFinished(board) || isRowFinished(board) || isDiagonalFinished(board))
-        return true
-    else if (isComplete(board))
-        return false
-    else
-        return false;
+function isRowFinished(board) {
+  return (board[0] == board[1] && board[1] == board[2]) ||
+    (board[3] == board[4] && board[4] == board[5]) ||
+    (board[6] == board[7] && board[7] == board[8])
+    ? true
+    : false;
 }
 
-function isComplete(board){
-    let isComplete = true;
-    for (let i = 0; i < board.length; i++) {
-        if (typeof(board[i]) === 'number') {
-             isComplete = false
-        };
-    };
-
-    return isComplete
+function isColumnFinished(board) {
+  return (board[0] == board[3] && board[3] == board[6]) ||
+    (board[1] == board[4] && board[4] == board[7]) ||
+    (board[2] == board[5] && board[5] == board[8])
+    ? true
+    : false;
 }
-
-function isRowFinished(board){
-    if (board[0] == board[1] && board[1] == board[2])
-    return true;
-    if (board[3] == board[4] && board[4] == board[5])
-        return true;
-    if (board[6] == board[7] && board[7] == board[8])
-        return true;
-
-    return false;
-}
-
-function isColumnFinished(board){
-    if ((board[0] == board[3] && board[3] == board[6]))
-        return true
-    if (board[1] == board[4] && board[4] == board[7])
-        return true
-    if (board[2] == board[5] && board[5] == board[8])
-        return true
-    
-    return false
-}
-function isDiagonalFinished(board){
-    if (board[0] == board[4] && board[4] == board[8])
-        return true
-    if (board[2] == board[4] && board[4] == board[6])
-        return true
-    
-    return false
+function isDiagonalFinished(board) {
+  return (board[0] == board[4] && board[4] == board[8]) ||
+    (board[2] == board[4] && board[4] == board[6])
+    ? true
+    : false;
 }
 Game(board);
 module.exports = {
-    Game,
-    drawBoard,
-    player,
-    switchPlayersTurns,
-    switchPlayersCharacters,
-    randomPosition,
-    isRowFinished,
-    isColumnFinished,
-    isDiagonalFinished,
-    isStillOnPlay
-  };
+  Game,
+  drawBoard,
+  player,
+  switchPlayersTurns,
+  switchPlayersCharacters,
+  randomPosition,
+  isRowFinished,
+  isColumnFinished,
+  isDiagonalFinished,
+  isStillOnPlay,
+};
--- tictactoe/tictactoe.test.js
+++ tictactoe/tictactoe.test.js
@@ -9,25 +9,6 @@ const { Game,
         isStillOnPlay } = require("./tictactoe");
 
 describe("TicTacToe", () => {
-    describe('Board and Draw Board', () => {
-        // it("Board - Gives an Empty Board", () => {
-        //     expect(Game([])).toEqual([]);
-        //   });
-        //   it("Board - Gives a board with empty values", () => {
-        //     expect(Game([0,1,2,3,4,5,6,7,8])).toEqual([0,1,2,3,4,5,6,7,8]);
-        //   });
-        
-        // it("Draw board - draw board as a tictactoe board", () => {
-        //         testBoard = ['','','','','','','','','']
-        //         expect(drawBoard(testBoard)).toEqual(`        |  ${testBoard[0]}  |  ${testBoard[1]}  |  ${testBoard[2]} \n
-        //         ----------- \n
-        //         |  ${testBoard[3]}  |  ${testBoard[4]}  |  ${testBoard[5]} \n
-        //         ----------- \n
-        //         |  ${testBoard[6]}  | ${testBoard[7]}   | ${testBoard[8]} \n
-        //     ` );
-        //     });
-    });
-
     describe('Game players', () => {
         it("Should return which player turn is it", () => {
             expect(switchPlayersTurns(1)).toEqual(1);
