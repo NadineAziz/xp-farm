8634ae34cc9faa3cc79d35d57b2b689a69a90fcb
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-09-09 14:35:28 +0200

    first test

--- what-is-between/whatIsBetween.js
+++ what-is-between/whatIsBetween.js
@@ -1,7 +1,11 @@
-function whatIsBetween(){
-
+function whatIsBetween(a,b){
+	let arr = [];
+	arr.push(a);
+	arr.push(b);
+	console.log(arr)
+	return arr;
 }
-
+whatIsBetween(1,2);
 module.exports = {
 	whatIsBetween
 }
