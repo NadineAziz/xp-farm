67517db1afd0cdc9acdb128346bd29d7e285fa25
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-09-09 14:36:29 +0200

    Revert "first test"
    This reverts commit 8634ae34cc9faa3cc79d35d57b2b689a69a90fcb.

--- what-is-between/whatIsBetween.js
+++ what-is-between/whatIsBetween.js
@@ -1,11 +1,7 @@
-function whatIsBetween(a,b){
-	let arr = [];
-	arr.push(a);
-	arr.push(b);
-	console.log(arr)
-	return arr;
+function whatIsBetween(){
+
 }
-whatIsBetween(1,2);
+
 module.exports = {
 	whatIsBetween
 }
--- what-is-between/whatIsBetween.test.ts
+++
@@ -1,7 +0,0 @@
-const { whatIsBetween }  = require( "./whatIsBetween");
-
-describe('WhatsInBetween',() => {
-	it('should return same number', () => {
-		expect(whatIsBetween(1,2)).toStrictEqual([1,2]);
-	})
-})
