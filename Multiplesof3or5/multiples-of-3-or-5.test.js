cc48b2d6db103f493610ffd37caddc3e3dd0b252
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-09-13 13:16:41 +0200

    Test 3: multiple of 5 failed test

--- Multiplesof3or5/multiples-of-3-or-5.test.js
+++ Multiplesof3or5/multiples-of-3-or-5.test.js
@@ -7,4 +7,10 @@ describe('Multiples of 3 or 5', () => {
 	it('Should return true if multiples of 3 up to a number' , () => {
 		expect(multiples3or5(20)).toBe(63);
 	});
+
+
+	it('Should return true if multiples of 5 up to a number' , () => {
+		expect(multiples3or5(20)).toBe(30);
+	});
+	
 })
