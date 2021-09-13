870e11f44916e23d9631acc1f1f9b76d6c53671a
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-09-13 13:36:47 +0200

    Test 4: return correct values from both 3 and 5

--- Multiplesof3or5/multiples-of-3-or-5.js
+++ Multiplesof3or5/multiples-of-3-or-5.js
@@ -1,13 +1,20 @@
 function multiples3or5 (num) {
+	let set = new Set();
 	let sum = 0;
 
 	for (let i = 0; i < num; i++) {
-	  if (i % 3 === 0) {
-		sum += i;
+	  if (i % 3 === 0 || i % 5 === 0) {
+		set.add(i);
 	  }
+	}
+
+	for(let i of set){
+		sum+=i;
 	}
 	return sum;
 }
+
+console.log(multiples3or5(10));
 module.exports = {
 	multiples3or5
 }
--- Multiplesof3or5/multiples-of-3-or-5.test.js
+++ Multiplesof3or5/multiples-of-3-or-5.test.js
@@ -1,16 +1,27 @@
 const { multiples3or5 } = require("./multiples-of-3-or-5");
 
 describe('Multiples of 3 or 5', () => {
-	it('Should return true if multiples of 3 up to a number' , () => {
-		expect(multiples3or5(10)).toBe(18);
+	it('Should return 3 if given 3' , () => {
+		expect(multiples3or5(3)).toBe(0);
 	});
-	it('Should return true if multiples of 3 up to a number' , () => {
-		expect(multiples3or5(20)).toBe(63);
+	it('Should return 23 if given 10' , () => {
+		expect(multiples3or5(10)).toBe(23);
 	});
 
+	it('Should return 8 if given 8' , () => {
+		expect(multiples3or5(8)).toBe(14);
+	});
 
-	it('Should return true if multiples of 5 up to a number' , () => {
-		expect(multiples3or5(20)).toBe(30);
+	it('Should return 195 if given 30' , () => {
+		expect(multiples3or5(50)).toBe(543);
+	});
+
+	it('Should return 2 if given 0' , () => {
+		expect(multiples3or5(2)).toBe(0);
+	});
+
+	it('Should return 120 if given 30' , () => {
+		expect(multiples3or5(120)).toBe(3300);
 	});
 	
 })
