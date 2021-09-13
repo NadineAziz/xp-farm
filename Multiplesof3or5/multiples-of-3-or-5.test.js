b1d263ec3aecaf9e806e4a6bfd81632a835531e7
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-09-13 13:15:09 +0200

    Test 2: multiple of 3 failed test

--- Multiplesof3or5/multiples-of-3-or-5.test.js
+++ Multiplesof3or5/multiples-of-3-or-5.test.js
@@ -1,7 +1,10 @@
 const { multiples3or5 } = require("./multiples-of-3-or-5");
 
 describe('Multiples of 3 or 5', () => {
-	it('Should return same number', () => {
-		expect(multiples3or5(1)).toBe(1);
-	})
+	it('Should return true if multiples of 3 up to a number' , () => {
+		expect(multiples3or5(10)).toBe(18);
+	});
+	it('Should return true if multiples of 3 up to a number' , () => {
+		expect(multiples3or5(20)).toBe(63);
+	});
 })
