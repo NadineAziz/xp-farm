9cd9924e8de77d65e9e00b8ed39bb1e46a03cb7e
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-09-13 13:15:38 +0200

    Test 2: multiple of 3 passed test

--- Multiplesof3or5/multiples-of-3-or-5.js
+++ Multiplesof3or5/multiples-of-3-or-5.js
@@ -1,5 +1,12 @@
 function multiples3or5 (num) {
-	return num;
+	let sum = 0;
+
+	for (let i = 0; i < num; i++) {
+	  if (i % 3 === 0) {
+		sum += i;
+	  }
+	}
+	return sum;
 }
 module.exports = {
 	multiples3or5
