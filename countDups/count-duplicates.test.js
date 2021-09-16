8a96285a009794c2873a869bf0fd64363759294e
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-09-16 09:56:39 +0200

    second test: count repeated letters failed

--- countDups/count-duplicates.test.js
+++ countDups/count-duplicates.test.js
@@ -5,5 +5,11 @@ describe('Counting Duplicates', () => {
 	it('Should return the text in lower case', () => {
 		expect(duplicateCount('AABbbb')).toBe('aabbbb');
 		expect(duplicateCount('TABles')).toBe('tables');
-	})
+	});
+
+	it('Should return count of the repeated letter', () => {
+		expect(duplicateCount('aaaaa')).toBe(5);
+	});
+
+	
 })
