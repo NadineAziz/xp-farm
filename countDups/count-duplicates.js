60e3532be9f837e7e6b6c00a353c4b057455bb78
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-09-16 15:10:37 +0200

    second test: counting pass

--- countDups/count-duplicates.js
+++ countDups/count-duplicates.js
@@ -1,7 +1,16 @@
 function duplicateCount(text){
-	return text.toLowerCase();
+	text = text.toLowerCase();
+	let count = 0;
+
+	for(let i=0;i<text.length;i++){
+		if(text[i] === text[i+1]){
+			count++;
+		}
+	}
+
+	return count;
 }
 
 module.exports = {
-	duplicateCount
-}
+	duplicateCount,
+  };
--- countDups/count-duplicates.test.js
+++ countDups/count-duplicates.test.js
@@ -2,13 +2,14 @@ const {duplicateCount} = require('./count-duplicates');
 
 describe('Counting Duplicates', () => {
 
-	it('Should return the text in lower case', () => {
-		expect(duplicateCount('AABbbb')).toBe('aabbbb');
-		expect(duplicateCount('TABles')).toBe('tables');
-	});
+	// it('Should return the text in lower case', () => {
+	// 	expect(duplicateCount('AABbbb')).toBe('aabbbb');
+	// 	expect(duplicateCount('TABles')).toBe('tables');
+	// });
 
 	it('Should return count of the repeated letter', () => {
 		expect(duplicateCount('aaaaa')).toBe(5);
+		expect(duplicateCount('aaaaabb')).toBe(5);
 	});
 
 	
