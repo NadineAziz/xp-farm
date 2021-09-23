8580a507ed7a93c85a46d106242035c6d49108bc
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-09-23 13:43:10 +0200

    remove pete the baker

--- pete-the-baker/cakes.js
+++
@@ -1,9 +0,0 @@
-function cakes(recipe, available) {
-    recipe = {flour: 500, sugar: 200, eggs: 1};
-
-  return recipe;
-}
-
-module.exports = {
-  cakes,
-};
--- pete-the-baker/cakes.test.js
+++
@@ -1,7 +0,0 @@
-const {cakes} = require('./cakes');
-
-describe('Pete the Baker',() => {
-	it('Should return recipe',() => {
-		expect(cakes({flour: 500, sugar: 200, eggs: 1})).toStrictEqual({flour: 500, sugar: 200, eggs: 1})
-	})
-})
