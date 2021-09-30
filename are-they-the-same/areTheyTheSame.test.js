a6960e948cfbefe703d25f4353dbe8438a7c5674
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-09-30 17:04:37 +0200

    Scenario 5 : Test fail

--- are-they-the-same/areTheyTheSame.test.js
+++ are-they-the-same/areTheyTheSame.test.js
@@ -1,15 +1,22 @@
 const { areTheyTheSame } = require("./areTheyTheSame");
 
 describe("Are they the same?", () => {
-  describe("User Story 1: Arrays with single elements", () => {
+  describe("User Story 1: Arrays with Initial elements", () => {
     it("Scenario 1 : should return false when both arrays are empty", () => {
       expect(areTheyTheSame([], [])).toBe(false);
     });
 
-    it("Scenario 2 : should return true when both numbers are equal", () => {
+    it("Scenario 2 & 3 : should return true when both numbers are equal", () => {
       expect(areTheyTheSame([1],[1])).toBe(true);
       expect(areTheyTheSame([2],[2])).toBe(true);
     });
     
+    it("Scenario 4 : should return true when both arrays have equal elements", () => {
+      expect(areTheyTheSame([2,3,4],[2,1])).toBe(true);
+    });
+
+    it("Scenario 5 : Given 2 arrays a=[2] b=[4] when I try to compare them then gives true", () => {
+      expect(areTheyTheSame([2],[4])).toBe(true);
+    });
   });
 });
