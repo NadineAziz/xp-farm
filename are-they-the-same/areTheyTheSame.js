1d1d6a79deaa3aa154b52360384e6021de7ff1e7
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-09-30 17:51:00 +0200

    US-2: Scenarios passed

--- are-they-the-same/areTheyTheSame.js
+++ are-they-the-same/areTheyTheSame.js
@@ -1,24 +1,26 @@
 function areTheyTheSame(array1, array2) {
-	let booleanArr = [];
-	for(let i in array1) {
-		for(let j in array2) {
-			if(array1.includes(Math.sqrt(array2[j]))){
-				booleanArr.push(true);
-			}else{
-				booleanArr.push(false);
-			}
-		}
-	}
-	for(let i in booleanArr){
-		if(booleanArr.includes(false)){
-			return false;
-		}
-	}
-	return true;
+  let booleanArr = [];
+  for (let j in array2) {
+    if (array1.includes(Math.sqrt(array2[j]))) {
+      booleanArr.push(true);
+    } else {
+      booleanArr.push(false);
+    }
+  }
+  if (booleanArr.includes(false)) {
+    return false;
+  }
+  console.log(booleanArr);
+  return true;
 }
 
 // console.log(areTheyTheSame([121, 144, 19, 161, 19, 144, 19, 11],[132, 14641, 20736, 361, 25921, 361, 20736, 361]));
-console.log(areTheyTheSame([121, 144, 19, 161, 19, 144, 19, 11], [121, 14641, 20736, 361, 25921, 361, 20736, 361]));
+console.log(
+  areTheyTheSame(
+    [0, 2, 9, 7, 0, 1, 3, 2, 6, 9, 0, 2, 10, 8, 10, 6],
+    [81, 4, 0, 1, 9, 100, 81, 49, 36, 100, 0, 4, 64, 1, 4, 36]
+  )
+);
 module.exports = {
-	areTheyTheSame
-}
+  areTheyTheSame,
+};
