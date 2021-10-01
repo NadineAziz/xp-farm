a6cc425e4d29eee95447fb1514b5ca8fa98524e3
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-10-01 11:42:31 +0200

    Test 2 fail

--- square-every-digit/squareEveryDigit.test.js
+++ square-every-digit/squareEveryDigit.test.js
@@ -4,4 +4,9 @@ describe("Square Every Number", () => {
   it("Given a single digit number 1 when I want to concatenate the number then it will be squared", () => {
     expect(squareEveryDigit(1)).toEqual(1);
   });
+
+  it("Given a single digit number 9 when I want to concatenate the number then it will be squared", () => {
+    expect(squareEveryDigit(9)).toEqual(81);
+  });
+
 });
