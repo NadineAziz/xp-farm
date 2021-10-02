259fa1e39a674a3a5147c2a431d562149a50e3cb
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-10-02 22:28:39 +0200

    Test 3 fail

--- sum-of-numbers/sum-of-numbers.test.js
+++ sum-of-numbers/sum-of-numbers.test.js
@@ -2,9 +2,14 @@ const { sumOfNumbers } = require("./sum-of-numbers");
 
 describe("Sum of Numbers", () => {
   it("Given a=0 and b=0 when I sum them up then should give me 0", () => {
-	  expect(sumOfNumbers(0,0)).toEqual(0);
+    expect(sumOfNumbers(0, 0)).toEqual(0);
   });
+
   it("Given a=1 and b=0 when I sum them up then should give me 1", () => {
-	expect(sumOfNumbers(1,0)).toEqual(1);
-});
+    expect(sumOfNumbers(1, 0)).toEqual(1);
+  });
+
+  it("Given a=1 and b=2 when I sum them up then should give me 3", () => {
+    expect(sumOfNumbers(1, 2)).toEqual(3);
+  });
 });
