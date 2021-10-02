a2ca325479607f52e2e6785657c02b3c1347092e
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-10-02 22:26:23 +0200

    Test 2 fail

--- sum-of-numbers/sum-of-numbers.test.js
+++ sum-of-numbers/sum-of-numbers.test.js
@@ -4,4 +4,7 @@ describe("Sum of Numbers", () => {
   it("Given a=0 and b=0 when I sum them up then should give me 0", () => {
 	  expect(sumOfNumbers(0,0)).toEqual(0);
   });
+  it("Given a=1 and b=0 when I sum them up then should give me 1", () => {
+	expect(sumOfNumbers(1,0)).toEqual(1);
+});
 });
