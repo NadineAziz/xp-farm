19250918b69c0e22da4cdbc286d762a3ff756d3a
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-10-04 10:06:02 +0200

    Tests 13 pass

--- number-big-enough/number-is-big-enough.test.js
+++ number-big-enough/number-is-big-enough.test.js
@@ -21,4 +21,36 @@ describe('Number is big enough', () => {
 	it('Given that I have "153" as an input when I square it\'s\ digits then it fails',()=>{
 		expect(narcissistic(153)).toBe(true);
 	});
+
+	it('Given that I have "370" as an input when I square it\'s\ digits then it fails',()=>{
+		expect(narcissistic(370)).toBe(true);
+	});
+
+	it('Given that I have "322" as an input when I square it\'s\ digits then it fails',()=>{
+		expect(narcissistic(322)).toBe(false);
+	});
+
+	it('Given that I have "1634" as an input when I square it\'s\ digits then it fails',()=>{
+		expect(narcissistic(1634)).toBe(true);
+	});
+
+	it('Given that I have "8208" as an input when I square it\'s\ digits then it fails',()=>{
+		expect(narcissistic(8208)).toBe(true);
+	});
+
+	it('Given that I have "8218" as an input when I square it\'s\ digits then it fails',()=>{
+		expect(narcissistic(8218)).toBe(false);
+	});
+
+	it('Given that I have "92727" as an input when I square it\'s\ digits then it fails',()=>{
+		expect(narcissistic(92727)).toBe(true);
+	});
+
+	it('Given that I have "1741725" as an input when I square it\'s\ digits then it fails',()=>{
+		expect(narcissistic(1741725)).toBe(true);
+	});
+
+	it('Given that I have "1742725" as an input when I square it\'s\ digits then it fails',()=>{
+		expect(narcissistic(1742725)).toBe(false);
+	});
 })
