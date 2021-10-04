a3b8f091a84844dda2ecae72eacf2fb2aa3b144a
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-10-04 09:52:45 +0200

    Test 2 setup pass

--- number-big-enough/number-is-big-enough.test.js
+++ number-big-enough/number-is-big-enough.test.js
@@ -5,4 +5,8 @@ describe('Number is big enough', () => {
 	it('Given that I have "1" as an input when I square it\'s\ digits then it passes',()=>{
 		expect(narcissistic(1)).toBe(true);
 	});
+
+	it('Given that I have "2" as an input when I square it\'s\ digits then it passes',()=>{
+		expect(narcissistic(2)).toBe(true);
+	});
 })
