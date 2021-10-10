a6c30478be6597f9890801264dead8c88a493311
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-10-11 00:01:31 +0200

    Test 2 pass

--- pandemia/pandemia.js
+++ pandemia/pandemia.js
@@ -1,6 +1,6 @@
 function pandemia(s) {
 	let ans = 0;
-	if(s === '0'){
+	if(s === '0' || s === 'X'){
 		ans= Math.round(0 * 100);
 	}
 	return ans;
--- pandemia/pandemia.test.js
+++ pandemia/pandemia.test.js
@@ -3,5 +3,9 @@ const {pandemia} = require('./pandemia');
 describe('Pandemia',()=>{
 	it('Given a map with 0 when I want to find the percentage then I give 0%',() => {
 		expect(pandemia('0')).toBe(0);
+	});
+
+	it('Given a map with X when I want to find the percentage then I give 0%',() => {
+		expect(pandemia('X')).toBe(0);
 	})
 })
