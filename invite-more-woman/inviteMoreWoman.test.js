4f685c07b556866455b4b07fbb8e494e649b5e9c
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-10-14 10:50:17 +0200

    Test 6: failing

--- invite-more-woman/inviteMoreWoman.test.js
+++ invite-more-woman/inviteMoreWoman.test.js
@@ -17,13 +17,19 @@ describe('Authur Party' , () => {
 		});
 	});
 
-	describe('As King Arthur has a party He wants to invite more men than women So that so he could have the chance for a date', () => {
+	describe('Senario 3: As King Arthur has a party He wants to invite more men than women So that so he could have the chance for a date', () => {
 		it('User Story 1: Given that there is a party When king author invites 2 men and 1 woman Then it should fail because she might not like him (he doesn\'t share sushi)',() => {
 			expect(inviteMoreWoman([1,1,-1])).toBe(false);
 		});
 
 		it('User Story 1: Given that there is a party When king author invites 5 men and 3 woman Then it should fail because Authur is eating sushi and getting all the women)',() => {
 			expect(inviteMoreWoman([1,1,1,1,1,-1,-1,-1])).toBe(false);
+		});
+	});
+
+	describe('Senario 4:As King Arthur has a party He wants to invite equal amount of men and women So that so he could have the chance for a date', () => {
+		it('User Story 1: Given that there is a party When king author invites 2 men and 1 woman Then it should fail because she might not like him (he doesn\'t share sushi)',() => {
+			expect(inviteMoreWoman([1,1,-1,-1])).toBe(true);
 		});
 	});
 	
