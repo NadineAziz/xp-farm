f727a0f5b7f5e3397fbe9c1ba348e9c80d59b3dd
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-10-14 10:45:30 +0200

    Test 2: passing

--- invite-more-woman/inviteMoreWoman.test.js
+++ invite-more-woman/inviteMoreWoman.test.js
@@ -4,6 +4,13 @@ describe('Authur Party' , () => {
 	describe('Senario 1: As King Arthur has a party He wants to invite no one So that so he could eat all the sushi',() => {
 		it('User Story 1:Given that there is a party When king author invites no one Then it should fail because he will have no date',()=>{
 			expect(inviteMoreWoman([])).toBe(false);
-		})
-	})
+		});
+	});
+
+	describe('Senario 2: As King Arthur has a party He wants to invite only men So that so he could eat all the sushi and they don\'t dare', () => {
+		it('User Story 1: Given that there is a party When king author invites one man Then it should fail because he will have no date',() => {
+			expect(inviteMoreWoman([1])).toBe(false);
+		});
+	});
+	
 });
