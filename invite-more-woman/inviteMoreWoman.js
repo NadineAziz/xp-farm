3ba2000d017d7402eadbc2f49157de2063e08e37
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-10-14 10:42:18 +0200

    Test 1: failing

---
+++ invite-more-woman/inviteMoreWoman.js
@@ -0,0 +1,7 @@
+function inviteMoreWoman(L){
+	return true;
+}
+
+module.exports = {
+	inviteMoreWoman,
+}
--- invite-more-woman/inviteMoreWoman.test.js
+++ invite-more-woman/inviteMoreWoman.test.js
@@ -1,3 +1,9 @@
-function inviteMoreWoman(L){
-	
-}
+const { inviteMoreWoman } = require("./inviteMoreWoman");
+
+describe('Authur Party' , () => {
+	describe('Senario 1: As King Arthur has a party He wants to invite no one So that so he could eat all the sushi',() => {
+		it('User Story 1:Given that there is a party When king author invites no one Then it should fail because he will have no date',()=>{
+			expect(inviteMoreWoman([])).toBe(false);
+		})
+	})
+});
