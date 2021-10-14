56afc28accba45351483a44a6b02c1ced279ee4c
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-10-14 10:48:55 +0200

    Test 5: passing

--- invite-more-woman/inviteMoreWoman.test.js
+++ invite-more-woman/inviteMoreWoman.test.js
@@ -21,6 +21,10 @@ describe('Authur Party' , () => {
 		it('User Story 1: Given that there is a party When king author invites 2 men and 1 woman Then it should fail because she might not like him (he doesn\'t share sushi)',() => {
 			expect(inviteMoreWoman([1,1,-1])).toBe(false);
 		});
+
+		it('User Story 1: Given that there is a party When king author invites 5 men and 3 woman Then it should fail because Authur is eating sushi and getting all the women)',() => {
+			expect(inviteMoreWoman([1,1,1,1,1,-1,-1,-1])).toBe(false);
+		});
 	});
 	
 });
