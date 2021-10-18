430f23f01c954b047f989b762596391b3276de67
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-10-18 09:59:01 +0200

    Test 4: pass

--- jumping-number/jumpingNumber.js
+++ jumping-number/jumpingNumber.js
@@ -1,6 +1,13 @@
 function jumpingNumber(number){
+  numArray = [...number+''].map(n=>+n);
+
   if(number.toString().length < 2){
     return 'Jumping!!';
+  }
+  for(let i in numArray){
+    if(numArray[i] - 1 === Math.abs(1)){
+      return 'Jumping!!';
+    }
   }
   return 'Not!!';
 }
