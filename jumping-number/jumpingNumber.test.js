eddea7a04097b62a04a7b98d9f7b1dec45f2d702
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-10-18 09:50:04 +0200

    Test 2: pass

--- jumping-number/jumpingNumber.test.js
+++ jumping-number/jumpingNumber.test.js
@@ -3,5 +3,9 @@ const {jumpingNumber} = require('./jumpingNumber');
 describe('Jumping Number',() => {
   it('Scenario 1:Given a single digit number 8 when I check its digits then the result should be Jumping!!',() => {
     expect(jumpingNumber(8)).toEqual('Jumping!!');
+  });
+
+  it('Scenario 2:Given a number 0 when I check its digits then the result should be Jumping!!',() => {
+    expect(jumpingNumber(0)).toEqual('Jumping!!');
   })
 })
