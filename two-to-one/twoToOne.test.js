803143982d15fae7c17d0d5773c5557997d0d23c
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-10-20 13:53:12 +0200

    Test 2 fail

--- two-to-one/twoToOne.test.js
+++ two-to-one/twoToOne.test.js
@@ -4,6 +4,9 @@ describe('Two to One', () => {
 	describe('User Story 1:As I have 2 strings with single letters I want to apply a function on them so that I get a sorted unique list.',() => {
 		it(`Scenario 1:Given strings a and b when I sort/unique them then it should return 'ab'`,() => {
 			expect(twoToOne('a','b')).toBe('ab');
+		});
+		it(`Scenario 2:Given strings c and d when I sort/unique them then it should return 'cd'`,() => {
+			expect(twoToOne('c','d')).toBe('cd');
 		})
 	})
 })
