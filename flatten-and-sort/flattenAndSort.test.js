218726114ac95f5747e3b53bce56a156ddc9a8a3
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-10-23 21:04:02 +0200

    Test 3 fail

--- flatten-and-sort/flattenAndSort.test.js
+++ flatten-and-sort/flattenAndSort.test.js
@@ -11,6 +11,13 @@ describe('Flatten and Sort Array', () => {
 		it('Scenario 2:Given an empty array [[],[]] when I try to flatten and sort it then it gives back an empty array []',() => {
 			expect(flattenAndSort([[],[]])).toStrictEqual([]);
 		});
+	});
+
+	describe('User Story 2:As an array with single number I want to flatten it so that it returns a single element array', () => {
+		it('Scenario 1:Given a single element array [[],[1]] when I try to flatten and sort it then it gives back [1]',() => {
+			expect(flattenAndSort([[],[1]])).toStrictEqual([1]);
+		})
 	})
+	
 	
 })
