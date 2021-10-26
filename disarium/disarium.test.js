30a83219da1bfb3674b5256ca83587bf7fcc8f7f
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-10-27 01:34:56 +0200

    Test 2 pass

--- disarium/disarium.test.js
+++ disarium/disarium.test.js
@@ -1,9 +1,13 @@
-const { disarium } = require('./disarium');
+const { disarium } = require("./disarium");
 
-describe('Disarium',() => {
-	describe('User Story 1:As a single digit number When I want to power it and add the digit So that I check if it\'s a Disarium or not',() => {
-		it('Scenario 1: Given a single digit 1 when I power it then it is a Disarium (gives "Disarium !!")',() => { 
-			expect(disarium(1)).toBe('Disarium !!');
-		})
-	})
-})
+describe("Disarium", () => {
+  describe("User Story 1:As a single digit number When I want to power it and add the digit So that I check if it's a Disarium or not", () => {
+    it('Scenario 1: Given a single digit 1 when I power it then it is a Disarium (gives "Disarium !!")', () => {
+      expect(disarium(1)).toBe("Disarium !!");
+    });
+
+    it('Scenario 2: Given a single digit 7 when I power it then it is a Disarium (gives "Disarium !!")', () => {
+      expect(disarium(7)).toBe("Disarium !!");
+    });
+  });
+});
