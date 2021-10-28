47e1b813fa5039ff1357c839cdda6933bab80e43
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-10-28 05:41:26 +0200

    Test 2 pass

--- shortest-word/shortest-word.js
+++ shortest-word/shortest-word.js
@@ -1,7 +1,13 @@
-function shortestWord(sentence){
-	return 1;
+function shortestWord(sentence) {
+  sentenceArray = sentence.split(" ");
+  minLength = sentenceArray[0].length;
+  for (let i in sentenceArray) {
+    if (sentenceArray[i].length < minLength) {
+      minLength = sentenceArray[i].length;
+    }
+  }
+  return minLength;
 }
-
 module.exports = {
-	shortestWord
-}
+  shortestWord,
+};
