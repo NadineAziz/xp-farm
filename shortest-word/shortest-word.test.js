f8bf03efa92f71fce213ae8f204125803a5f05b7
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-10-28 05:38:50 +0200

    Test 2 fail

--- shortest-word/shortest-word.test.js
+++ shortest-word/shortest-word.test.js
@@ -5,5 +5,8 @@ describe("Shortest Word", () => {
     it("Scenario 1: Given a sentence with 1 word  ['w'] when I find the minimum length then I get length = 1", () => {
 		expect(shortestWord('w')).toBe(1);
 	});
+	it("Scenario 2: Given a sentence with 2 words ['cv np'] with the same length when I find the minimum length then I get length = 2", () => {
+		expect(shortestWord('cv np')).toBe(2);
+	});
   });
 });
