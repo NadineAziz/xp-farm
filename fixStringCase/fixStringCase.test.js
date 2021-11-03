75ae726db5fda07e8d92f05a8704bb751b29043a
Nadeen Hussein <nadeen.hussein1@ibm.com>
2021-11-03 05:17:46 +0100

    Test 2 pass

--- fixStringCase/fixStringCase.test.js
+++ fixStringCase/fixStringCase.test.js
@@ -1,11 +1,12 @@
-const {fixStringCase} = require('./fixStringCase');
+const { fixStringCase } = require("./fixStringCase");
 
-describe('Fix String Case', () => {
-	
-	describe('User Story 1:As a single character case string I want to check the upper and lower case letters so that I change the word accordingly',() => {
-
-		it(`Scenario 1:Given a single letter 'c' when I check the upper: lower case ratio then it returns 'c'`,() => {
-			expect(fixStringCase('c')).toBe('c');
-		})
-	});
-})
+describe("Fix String Case", () => {
+  describe("User Story 1:As a single character case string I want to check the upper and lower case letters so that I change the word accordingly", () => {
+    it(`Scenario 1:Given a single letter 'c' when I check the upper: lower case ratio then it returns 'c'`, () => {
+      expect(fixStringCase("c")).toBe("c");
+    });
+    it(`Scenario 2:Given a 1 letter 'T' when I check the upper: lower case ratio then it returns 'T'`, () => {
+      expect(fixStringCase("T")).toBe("T");
+    });
+  });
+});
